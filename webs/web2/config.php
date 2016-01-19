d322de7c6bfbe7904867ccfbdd671ea55c404cad
xtzlyp <xtzlyp@aliyun.com>
2016-01-19 12:59:16 +0800

    21

---
+++ webs/web2/config.php
@@ -0,0 +1,11 @@
+<?php
+	$__Config__ = array(
+			'tempConfig' => array(
+							'type' => 'smarty',
+							'left_delimiter' => '<{',
+							'right_delimiter' => '}>',
+							'tempFile' => 'templates'
+						)
+			 
+		);
+?>
--- webs/web2/index.php
+++ webs/web2/index.php
@@ -1,10 +1,5 @@
 <?php
 	include './../../lx/lx.php';
 	require './../../lx/view/smarty/Smarty.class.php';
-	$smarty = new Smarty;
-	$smarty->setTemplateDir("templates");
-	$smarty->setCacheDir("cache");
-	$smarty->left_delimiter = "<{"; 
-	$smarty->right_delimiter = "}>";
 	$smarty->display('index.tpl');
 ?>
